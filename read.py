ec790d76c31b4703c81012174809e646e07be9dc
Aditya Raj <adityaraj21122018@gmail.com>
2024-09-27 17:59:13 +0530

    update python
    20:04

--- read.py
+++ read.py
@@ -6,15 +6,25 @@ cv.imshow('Dog', img)
 cv.imshow('Dog Gray', cv.cvtColor(img, cv.COLOR_BGR2GRAY))
 
 def  rescaleFrame(frame, scale=0.75):
+    # work for image, video and live
     width = int(frame.shape[1]*scale)
     height = int(frame.shape[0]*scale)
     dimensions = (width, height)
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+def changeres(width, height):
+    # only work for live video
+    capture.set(3, width)
+    capture.set(4, height)
+
   # reading video
 capture = cv.VideoCapture('video/video11.mp4')
 while True:
     isTrue, frame = capture.read()
+    frame_resized = rescaleFrame(frame, scale=0.2)
     cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 capture.release()
